2a317ff77585d0a51fc243e5b71fc455389b6738
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-13 22:34:33 -0300

    Primeiro commit

---
+++ backend/src/main/java/com/robertosouza/descatalog/DescatalogApplication.java
@@ -0,0 +1,13 @@
+package com.robertosouza.descatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescatalogApplication.class, args);
+	}
+
+}
