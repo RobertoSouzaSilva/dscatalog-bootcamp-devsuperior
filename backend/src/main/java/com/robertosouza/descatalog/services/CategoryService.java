03b2e6f26db4d9afd11aa1a8d01899b831e4d8dc
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-14 22:26:40 -0300

    category dto

--- backend/src/main/java/com/robertosouza/descatalog/services/CategoryService.java
+++ backend/src/main/java/com/robertosouza/descatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.robertosouza.descatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.robertosouza.descatalog.dto.CategoryDTO;
 import com.robertosouza.descatalog.entities.Category;
 import com.robertosouza.descatalog.repositories.CategoryRepository;
 
@@ -14,8 +17,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = categoryRepository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 	
 }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_category (name) values ('books');
+insert into tb_category (name) values ('computers');
+insert into tb_category (name) values ('phones');
