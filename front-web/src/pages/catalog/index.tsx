5c395e25d0960ef6712164edb3b45e0cbb870beb
Roberto Souza <robertosouza57@yahoo.com.br>
2021-01-24 15:03:28 -0300

    detalhes do produto adicionado

--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ProductCard from './components/ProductCard/ProductCard';
+import { Link } from 'react-router-dom';
+import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => (
@@ -7,21 +8,26 @@ const Catalog = () => (
         <h1 className="catalog-title">Catálogo de produtos</h1>
 
         <div className="catalog-Products">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+            <Link to="/products/1"><ProductCard/></Link>
+            <Link to="/products/2"><ProductCard/></Link>
+            <Link to="/products/3"><ProductCard/></Link>
+            <Link to="/products/4"><ProductCard/></Link>
+            <Link to="/products/5"><ProductCard/></Link>
+            <Link to="/products/6"><ProductCard/></Link>
+            <Link to="/products/7"><ProductCard/></Link>
+            <Link to="/products/8"><ProductCard/></Link>
+            <Link to="/products/9"><ProductCard/></Link>
+            <Link to="/products/10"><ProductCard/></Link>
+            <Link to="/products/11"><ProductCard/></Link>
+            <Link to="/products/12"><ProductCard/></Link>
+            <Link to="/products/13"><ProductCard/></Link>
+            <Link to="/products/14"><ProductCard/></Link>
+            <Link to="/products/15"><ProductCard/></Link>
+            <Link to="/products/16"><ProductCard/></Link>
+            <Link to="/products/17"><ProductCard/></Link>
+            <Link to="/products/18"><ProductCard/></Link>
+            <Link to="/products/19"><ProductCard/></Link>
+            <Link to="/products/20"><ProductCard/></Link>
         </div>
     </div>
 
--- front-web/src/pages/home/index.tsx
+++ front-web/src/pages/home/index.tsx
@@ -10,7 +10,7 @@ const Home = () => (
             <div className="col-6">
                 <h1 className="text-title">Conheça o melhor <br/> catálogo de produtos</h1>
                 <p className="text-subtitle">Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.</p>
-                <Link to="/catalog"><ButtonIcon text="inicie agora a sua busca"/></Link>
+                <Link to="/products"><ButtonIcon text="inicie agora a sua busca"/></Link>
             </div>
             <div className="col-6">
                 <MainImage className="main-image"/>
