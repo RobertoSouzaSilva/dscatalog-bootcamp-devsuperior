cd7d6587477e6e0bfb5461877f6610deb219a45f
Roberto Souza <robertosouza57@yahoo.com.br>
2021-02-02 20:43:37 -0300

    add navbar

--- front-web/src/pages/admin/index.tsx
+++ front-web/src/pages/admin/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Navbar from './Admin/components';
 
 const Admin = () => (
     <div>
-        <h1>Admin</h1>
+        <Navbar/>
     </div>
 
 );
--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -6,41 +6,47 @@ import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoad from './components/ProductDetails/Loaders/ProductCardLoader';
 import './styles.scss';
+import Pagination from 'core/components/Pagination';
 
 const Catalog = () => {
 
-   const[productsResponse, setproductsResponse] = useState<ProductsResponse>();
-   const [isLoading, setIsLoading] = useState(false);
- 
-    useEffect(() =>{
+    const [productsResponse, setproductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setactivePage] = useState(0);
+
+
+    useEffect(() => {
         const params = {
-            page:0,
-            linesPerPage:30
+            page: activePage,
+            linesPerPage: 12
         }
         setIsLoading(true);
-        makeRequest({url: '/products', params})
-        .then(response => setproductsResponse(response.data))
-        .finally(() => {
-            setIsLoading(false);
-        })
-    }, []);
+        makeRequest({ url: '/products', params })
+            .then(response => setproductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [activePage]);
 
-    return(
+    return (
         <div className="catalog-container">
-        <h1 className="catalog-title">Catálogo de produtos</h1>
+            <h1 className="catalog-title">Catálogo de produtos</h1>
 
-        <div className="catalog-Products">
-            {isLoading ? <ProductCardLoad/> : (
-                productsResponse?.content.map(product =>
-                <Link to={`/products/${product.id}`} key={product.id}>
-                    <ProductCard product={product}/>
-                </Link>
+            <div className="catalog-Products">
+                {isLoading ? <ProductCardLoad /> : (
+                    productsResponse?.content.map(product =>
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
+                    )
                 )
-            )
-            }
-           
+                }
+
+            </div>
+            {productsResponse && (
+                <Pagination totalPages={productsResponse.totalPages} activePage={activePage} onChange={page => setactivePage(page)}/>
+            )}
         </div>
-    </div>
     );
 };
 
