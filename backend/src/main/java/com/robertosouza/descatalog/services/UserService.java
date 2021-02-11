853a0251c5eeeef364f36d92d9e06fd10557eb41
Roberto Souza <robertosouza57@yahoo.com.br>
2021-02-10 22:09:18 -0300

    oauth2 e authorization server implementados

--- backend/src/main/java/com/robertosouza/descatalog/services/UserService.java
+++ backend/src/main/java/com/robertosouza/descatalog/services/UserService.java
@@ -5,11 +5,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +30,9 @@ import com.robertosouza.descatalog.repositories.UserRepository;
 import com.robertosouza.descatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -105,6 +112,18 @@ public class UserService {
 			userEntity.getRoles().add(role);
 		}
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userRepository.findByEmail(username);
+		if(user == null) {
+			logger.error("usuario nao encontrado " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("usuario encontrado " + username);
+		return user;
 	}
 	
 	
