9a5c8d4952060dc141f58d5898f7684689331516
Roberto Souza <robertosouza57@yahoo.com.br>
2021-02-13 16:47:38 -0300

    primeiro teste de upload

--- backend/src/main/java/com/robertosouza/descatalog/DescatalogApplication.java
+++ backend/src/main/java/com/robertosouza/descatalog/DescatalogApplication.java
@@ -1,13 +1,26 @@
 package com.robertosouza.descatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DescatalogApplication {
+import com.robertosouza.descatalog.services.S3Service;
 
+@SpringBootApplication
+public class DescatalogApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private S3Service s3;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DescatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3.uploadFile("C:\\wallpaperflare.com_wallpaper.jpg");
 	}
 
 }
