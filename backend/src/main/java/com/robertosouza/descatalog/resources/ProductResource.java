69c0b1a730c34e572154297c6884b3088bf63855
Roberto Souza <robertosouza57@yahoo.com.br>
2021-02-12 22:28:42 -0300

    filtragem por catregoria e nome do produto e compatibilidade com postgres

--- backend/src/main/java/com/robertosouza/descatalog/resources/ProductResource.java
+++ backend/src/main/java/com/robertosouza/descatalog/resources/ProductResource.java
@@ -38,6 +38,8 @@ public class ProductResource {
 	
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
+			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
+			@RequestParam(value = "name", defaultValue = "") String name,
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
@@ -45,7 +47,7 @@ public class ProductResource {
 			){
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<ProductDTO> list = productService.findAllPaged(pageRequest);
+		Page<ProductDTO> list = productService.findAllPaged(categoryId, name, pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/robertosouza/descatalog/services/ProductService.java
+++ backend/src/main/java/com/robertosouza/descatalog/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.robertosouza.descatalog.services;
 
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -37,8 +39,11 @@ public class ProductService {
 //	}
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		Page<Product> list = productRepository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest){
+		
+		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+		
+		Page<Product> list = productRepository.find(categories, name.trim(), pageRequest);
 		return list.map(x -> new ProductDTO(x));
 	}
 
