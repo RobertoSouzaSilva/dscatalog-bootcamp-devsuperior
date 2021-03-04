bf9d2f83ce8a5360b2555c3aaf87d87595dd14ce
Roberto Souza <robertosouza57@yahoo.com.br>
2021-03-03 22:33:38 -0300

    login e feito e salvamento dos dados da sessão do usuario

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,19 +1,48 @@
 import axios,{Method} from 'axios';
+import qs from 'qs';
+import { CLIENT_ID, CLIENT_SECRET } from './auth';
 
 type RequestParams = {
     method?: Method;
     url: string;
-    data?: object;
+    data?: object | string;
     params?: object;
+    headers?: object;
 }
 
-const BASE_URL='http://localhost:3000'
+type LoginData = {
+    username: string;
+    password: string;
+}
 
-export const makeRequest = ({method = 'GET', url, data, params}: RequestParams) => {
+const BASE_URL='http://localhost:8080'
+
+export const makeRequest = ({method = 'GET', url, data, params, headers}: RequestParams) => {
     return axios({
         method,
         url: `${BASE_URL}${url}`,
         data,
-        params
+        params,
+        headers
     });
+}
+
+//yarn add qs
+//yarn add @types/qs
+
+export const makeLogin = (longinData: LoginData) => {
+    const token = `${CLIENT_ID}:${CLIENT_SECRET}`
+
+    const headers = {
+        Authorization: `Basic ${window.btoa(token)}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const payload = qs.stringify({...longinData, grant_type: 'password'});
+    //outra maneira de se fazer
+    //const payload = `username=${longinData.username}&password=${longinData.password}&grant_type=password`
+
+    return makeRequest({url: '/oauth/token', data: payload, method: 'POST', headers});
+
+
 }
--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import AuthCard from '../Card';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import ButtonIcon from 'core/components/ButtonIcon';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
 
     const { register, handleSubmit, errors } = useForm<FormData>(); 
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
-        console.log(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false); 
+                saveSessionData(response.data);
+                history.push('/admin');
+                })
+            .catch(() => {setHasError(true)});
     }
 
     return (
         <AuthCard title="Login">
+            {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos!
+                </div>
+            )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                <input name="email" type="email" ref={register} className="form-control input-base margin-bottom-30" placeholder="Email"/>
-                <input name="password" type="password" ref={register} className="form-control input-base" placeholder="Senha"/>
+                <input name="username" type="email" ref={register({required:true})} className="form-control input-base margin-bottom-30" placeholder="Email"/>
+                <input name="password" type="password" ref={register({required:true})} className="form-control input-base" placeholder="Senha"/>
                 <Link to="/admin/auth/recover" className="login-link-recover">Esqueci a senha?</Link>
                 <div className="login-submit">
                     <ButtonIcon text="Logar"/>
