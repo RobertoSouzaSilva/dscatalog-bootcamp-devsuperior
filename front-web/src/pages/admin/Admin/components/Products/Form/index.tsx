1a7257046dcd67fac7920f5f354faf1db37e5be7
Roberto Souza <robertosouza57@yahoo.com.br>
2021-03-09 21:03:24 -0300

    toasty quando salva um produto

--- front-web/src/pages/admin/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/admin/Admin/components/Products/Form/index.tsx
@@ -2,6 +2,9 @@ import { makePrivateRequest } from 'core/utils/request';
 import { useForm } from 'react-hook-form';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router';
+
 
 type FormState = {
     name: string,
@@ -14,11 +17,19 @@ type FormState = {
 
 const Form = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
+    const history = useHistory();
 
 
     const onSubmit = (data: FormState) => {
         console.log(data);
-        makePrivateRequest({ url: '/products', method: 'POST', data });
+        makePrivateRequest({ url: '/products', method: 'POST', data })
+            .then(() => {
+                toast.info('Produto salvo com sucesso!');
+                history.push('admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto!');
+            })
 
     }
 
